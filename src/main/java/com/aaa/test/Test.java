386095252bb575e5299c4df546862ac9d77441ef
kmxiao <18676669765@163.com>
2020-12-02 00:16:48 +0800

    修改项目名

--- src/main/java/com/aaa/test/Test.java
+++ src/main/java/com/aaa/test/Test.java
@@ -10,7 +10,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("红色的说明并没有放到暂存区 可以通过右键项目名 找到git 然后add");
-        System.out.println("绿色的说明添加到暂存区 接着通过git commit filles");
-        System.out.println("正常颜色说明已经添加到版本库");
+        System.out.println("绿色的说明添加到暂存区 接着通过git commit files");
+        System.out.println("正常颜色说明已经添加到版本库 jie这pull");
     }
 }
---
+++ src/main/java/com/aaa/test/Test02.java
@@ -0,0 +1,11 @@
+package com.aaa.test;
+
+/**
+ * @program: springboot
+ * @description
+ * @author: lc
+ * @create: 2020-12-02 00:12
+ **/
+public class Test02 {
+
+}
